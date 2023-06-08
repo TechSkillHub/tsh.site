b3239eeec5e1212c42dac13ead72b0ce864058ce
Felipe Carvalho <felipe.carvalho@fullbar.srv>
2023-06-08 18:07:00 -0300

    Install

---
+++ src/components/banner/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="banner">
+    <h1>EM BREVE!</h1>
+    <h3>A melhor plataforma para conectar profissionais de tecnologia.</h3>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  h1, h3 {
+    text-align: center;
+  }
+  h1 {
+    font-size: 200px;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="container d-flex justify-content-center align-items-center">
+    <Banner />
+  </main>
+</template>
+
+<script setup>
+import Banner from '../components/banner/Banner.vue'
+</script>
+
+<style lang="scss" scoped>
+main {
+  height: 100vh;
+}
+</style>
